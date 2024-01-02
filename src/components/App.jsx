a0e903a8a23b05af1615a1f66cf624d694c2f702
shubhamatkal <shubhamatkal333@gmail.com>
2024-01-02 15:03:24 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Entry from "./entry";
+import emojipedia  from "../emojipedia";
+
+function createEntry(emojiterm) {
+  return <Entry 
+    key = {emojiterm.id}
+    emoji = {emojiterm.emoji}
+    name = {emojiterm.name}
+    description = {emojiterm.meaning}
+  />
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">
+      {emojipedia.map(createEntry)}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
